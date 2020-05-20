1f3fb3aa7c13ab168b8fc028ac58d18e0534f040
user <user@DESKTOP-N74F5VP>
2020-05-20 06:39:19 -0700

    First Commit

---
+++ MyFirstMaven/src/test/java/TestHello.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+public class TestHello {
+
+		// TODO Auto-generated method stub
+@Test
+public void testMethod() {
+		System.out.println("Hello World");
+	}
+
+}
